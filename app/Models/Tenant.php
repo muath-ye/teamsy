b5cb8ea9a6f4e00d0dc5e721ded0dee4968af8b4
muathye <muath.ye@gmail.com>
2022-11-04 00:33:24 +0300

    :zap: update tenants model

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Tenant extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
 }
