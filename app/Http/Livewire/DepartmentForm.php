c48b826b8ff3399d40ba2f6c319b3e0ad3f72f5b
muathye <muath.ye@gmail.com>
2022-11-04 01:09:15 +0300

    :art: artisan make:livewire department-form

---
+++ app/Http/Livewire/DepartmentForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class DepartmentForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.department-form');
+    }
+}
