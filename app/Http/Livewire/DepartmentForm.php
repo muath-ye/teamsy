42c554826a66cae2b4b215f538c2d03ec1ccda24
muathye <muath.ye@gmail.com>
2022-11-04 01:14:22 +0300

    :zap: update department-form

--- app/Http/Livewire/DepartmentForm.php
+++ app/Http/Livewire/DepartmentForm.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Deparment;
 use Livewire\Component;
 
 class DepartmentForm extends Component
 {
+    public $name;
+    public function mount($departmentId = null)
+    {
+        if ($departmentId) {
+            $this->name = Deparment::findOrFail($departmentId)->name;
+        }
+    }
     public function render()
     {
         return view('livewire.department-form');
