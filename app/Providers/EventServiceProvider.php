8101088a5ef6079233f28e3ed63e9ede66b5f6c8
muathye <muath.ye@gmail.com>
2022-11-04 00:52:44 +0300

    :zap: listen SetTenantIdInSession

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Listeners\SetTenantIdInSession;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -18,6 +20,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        Login::class => [
+            SetTenantIdInSession::class,
+        ]
     ];
 
     /**
