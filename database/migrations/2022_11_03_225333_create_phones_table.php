2531db5d428f551af0380dfd16f27899508265bf
muathye <muath.ye@gmail.com>
2022-11-04 01:53:48 +0300

    :art: artisan make:model Phone -mf

---
+++ database/migrations/2022_11_03_225333_create_phones_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Tenant;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Tenant::class)->comment('to make this ->constrained() make tenants table before phones table');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+};
