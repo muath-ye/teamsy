ab1ac53c44c81c57cf8cf09af7e4770e6eca6242
muathye <muath.ye@gmail.com>
2022-11-04 01:38:18 +0300

    :zap: update department-form

--- resources/views/livewire/department-form.blade.php
+++ resources/views/livewire/department-form.blade.php
@@ -1,3 +1,5 @@
 <div>
-    {{-- Success is as dangerous as failure. --}}
+    <input type="text" wire:model="name" />
+    <button wire:click="submit">Submit</button>
+    @if($success)<div>Saved</div>@endif
 </div>
