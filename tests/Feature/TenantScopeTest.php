ee418b506c517eb4a011c74bec3cb8eb92f0af6d
muathye <muath.ye@gmail.com>
2022-11-04 02:45:05 +0300

    :zap: update TenantScopeTest

--- tests/Feature/TenantScopeTest.php
+++ tests/Feature/TenantScopeTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\Tenant;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Tests\TestCase;
 
@@ -28,5 +31,21 @@ class TenantScopeTest extends TestCase
         // clean up
         File::delete(database_path('migrations/'.$filename));
         File::delete(app_path('Models/Test.php'));
+    }
+
+    /** @test */
+    public function a_can_only_see_users_on_the_same_tenant()
+    {
+        // create two tenants
+        $tenant_1 = Tenant::factory()->create();
+        $tenant_2 = Tenant::factory()->create();
+        // create users for each tenant for example 10 users for each tenant
+        $user = User::factory()->create(['tenant_id' => $tenant_1->id]);
+        User::factory()->count(9)->create(['tenant_id' => $tenant_1->id]);
+        User::factory()->count(10)->create(['tenant_id' => $tenant_2->id]);
+        // login one users
+        Auth::login($user);
+        // assert logged in user should only see 10 users
+        $this->assertEquals(10, User::count());
     }
 }
