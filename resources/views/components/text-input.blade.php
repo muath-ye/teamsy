36565a7315b23137500d34409d5c043e86069533
muathye <muath.ye@gmail.com>
2022-11-04 06:01:29 +0300

    :zap: update text-input component

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -11,7 +11,7 @@
     </label>
 
     <div class="mt-1 rounded-md shadow-sm">
-        <input wire:model.debounce.lazy="{{$attributes->whereStartsWith('wire:model')->first()}}" id="{{$attributes->whereStartsWith('wire:model')->first()}}" type="text" {{$required}} autofocus class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error($attributes->whereStartsWith('wire:model')->first()) border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:ring-red @enderror" />
+        <input wire:model.debounce.lazy="{{$attributes->whereStartsWith('wire:model')->first()}}" id="{{$attributes->whereStartsWith('wire:model')->first()}}" type="{{$type}}" {{$required}} autofocus class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error($attributes->whereStartsWith('wire:model')->first()) border-red-300 text-red-900 placeholder-red-300 focus:border-red-300 focus:ring-red @enderror" />
     </div>
 
     @error($attributes->whereStartsWith('wire:model')->first())
