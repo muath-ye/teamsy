87e5fac5cc9c151c91cf7c9d53a26a1b9355fe2b
muathye <muath.ye@gmail.com>
2022-11-04 03:53:05 +0300

    :zap: update base layout with x-cloak

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -17,6 +17,7 @@
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        <style>[x-cloak] { display: none !important; }</style>
         @livewireStyles
         @livewireScripts
 
