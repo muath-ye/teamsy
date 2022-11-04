ca085e184e0ccdc40ea8c54451d481cca0f2141c
muathye <muath.ye@gmail.com>
2022-11-04 06:21:34 +0300

    :zap: update register component

--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Register extends Component
@@ -36,23 +37,34 @@ class Register extends Component
             'password' => ['required', 'min:8', 'same:passwordConfirmation'],
         ]);
 
-        $tenant = Tenant::create([
-            'name' => $this->company,
-        ]);
+        DB::beginTransaction();
+        try {
+            $tenant = Tenant::create([
+                'name' => $this->company,
+            ]);
 
-        $user = User::create([
-            'name' => $this->name,
-            'email' => $this->email,
-            'role' => 'admin', // Admin because this user is register the tenant and can add new users to the tenant
-            'tenant_id' => $tenant->tenant_id,
-            'password' => Hash::make($this->password),
-        ]);
+            $user = User::create([
+                'name' => $this->name,
+                'email' => $this->email,
+                'role' => 'admin', // Admin because this user is register the tenant and can add new users to the tenant
+                'tenant_id' => $tenant->tenant_id,
+                'password' => Hash::make($this->password),
+            ]);
 
+            DB::commit();
+        } catch (\Exception $exception) {
+            DB::rollBack();
+        }
         event(new Registered($user));
 
         Auth::login($user, true);
 
         return redirect()->intended(route('home'));
+    }
+
+    public function updated($property)
+    {
+        $this->resetErrorBag($property);
     }
 
     public function render()
