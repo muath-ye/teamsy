dab47a591d7170ec9e94d16ce9a538d852bc4dcb
muathye <muath.ye@gmail.com>
2022-11-05 05:58:23 +0300

    :art: add helpers.php

---
+++ resources/helpers.php
@@ -0,0 +1,27 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Application Helpers
+|--------------------------------------------------------------------------
+|
+| This the place of your helpers.php
+| Add your helpers functions here, so you can call any function in every
+| where of your project.
+|
+*/
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
+
+if (! function_exists('carbon')) {
+    function carbon($parseString = null, $tz = null)
+    {
+        return new Carbon($parseString, $tz);
+    }
+}
+
+if (! function_exists('storage')) {
+    function storage($parseString = null, $tz = null)
+    {
+        return new Storage($parseString, $tz);
+    }
+}
