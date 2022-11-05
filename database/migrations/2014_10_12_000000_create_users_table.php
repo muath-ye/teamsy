4d93db187135cf8528b699dc71cb79a19ab35421
muathye <muath.ye@gmail.com>
2022-11-05 04:16:32 +0300

    :zap: update user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,10 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('role')->nullable()->comment('you can use spatie package');
+            $table->string('photo')->nullable();
+            $table->string('department')->nullable();
+            $table->string('title')->nullable();
+            $table->boolean('status')->default(1);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
