94bb7d424b2d889c504f04ba0ddce2d78ab7357d
muathye <muath.ye@gmail.com>
2022-11-05 13:03:53 +0300

    :art: add Login migration

---
+++ database/migrations/2022_11_05_100150_create_logins_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Tenant;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logins', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Tenant::class)->index()->comment('to make this ->constrained() make tenants table before logins table');
+            $table->foreignIdFor(User::class)->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logins');
+    }
+};
