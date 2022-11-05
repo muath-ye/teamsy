76eeb75fbc94224761f3aa491e508c57d9e2f323
muathye <muath.ye@gmail.com>
2022-11-05 04:21:55 +0300

    :zap: update document model

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -9,4 +9,28 @@ use Illuminate\Database\Eloquent\Model;
 class Document extends Model
 {
     use HasFactory, BelongsToTenant;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'tenant_id',
+        'type',
+        'filename',
+        'extension',
+        'size',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function privateUrl()
+    {
+        return url('/documents/' . $this->user_id . '/' . $this->filename);
+    }
 }
